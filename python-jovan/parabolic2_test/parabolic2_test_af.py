65d14e51af2cf35f208b1f2b0150bb287cd43576
Liujiawangmath <15674760037@163.com>
2023-07-26 21:48:47 +0800

    jovan_parabolic_test

---
+++ python-jovan/parabolic2_test/parabolic2_test_af.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import spsolve
+from fealpy.mesh.uniform_mesh_2d import UniformMesh2d
+from fealpy.decorator import cartesian
+from typing import Tuple
+class SinSinExpPDEData: 
+    def __init__(self, D=[0, 1, 0, 1], T=[0, 0.1]):
+        
+        self._domain = D 
+        self._duration = T 
+
+    def domain(self):
+        
+        return self._domain
+
+    def duration(self):
+       
+        return self._duration 
+
+    @cartesian
+    def solution(self, p, t):
+       
+        pi = np.pi
+        x = p[..., 0]
+        y = p[..., 1]
+        return np.sin(pi*x)*np.sin(pi*y)*np.exp(-2*pi*t) 
+
+    @cartesian
+    def init_solution(self, p):
+        
+        pi = np.pi
+        x = p[..., 0]
+        y = p[..., 1]
+        return np.sin(pi*x)*np.sin(pi*y)
+        
+    @cartesian
+    def source(self, p, t):
+        
+        pi = np.pi
+        x = p[..., 0]
+        y = p[..., 1]
+        return np.zeros(x.shape)
+    
+    @cartesian
+    def gradient(self, p, t):
+       
+        x = p[..., 0]
+        y = p[..., 1]
+        pi = np.pi
+        val = np.zeros(p.shape, dtype=np.float64)
+        val[..., 0] = pi*np.cos(pi*x)*np.sin(pi*y)*np.exp(-2*pi*t)
+        val[..., 1] = pi*np.sin(pi*x)*np.cos(pi*y)*np.exp(-2*pi*t)
+        return val
+    
+    @cartesian    
+    def dirichlet(self, p, t):
+        
+        return self.solution(p, t)
+# PDE 模型
+pde = SinSinExpPDEData()
+# 空间离散
+domain = pde.domain()
+nx = 30
+ny = 30
+hx = (domain[1] - domain[0])/nx
+hy = (domain[3] - domain[2])/ny
+mesh = UniformMesh2d([0, nx, 0, ny], h=(hx, hy), origin=(domain[0], domain[2]))
+duration = pde.duration()
+nt = 1600 
+tau = (duration[1] - duration[0])/nt 
+#初值
+uh0 = mesh.interpolate(pde.init_solution, intertype='node')
+def advance_forward(n: np.int_) -> Tuple[np.ndarray, np.float64]: # 点击这里可以查看 FEALPy 仓库中的代码
+    """
+    @brief 时间步进格式为向前欧拉方法
+    
+    @param[in] n int, 表示第 n 个时间步（当前时间步） 
+    """
+    t = duration[0] + n*tau
+    if n == 0:
+        return uh0, t
+    else:
+        A = mesh.parabolic_operator_forward(tau)
+        source: Callable[[np.ndarray], np.ndarray] = lambda p: pde.source(p, t + tau)
+        f = mesh.interpolate(source, intertype='node')
+        uh0[:].flat = A@uh0[:].flat + (tau*f[:]).flat
+        gD: Callable[[np.ndarray], np.ndarray] = lambda p: pde.dirichlet(p, t + tau)
+        mesh.update_dirichlet_bc(gD, uh0)
+        
+        solution: Callable[[np.ndarray], np.ndarray] = lambda p: pde.solution(p, t + tau)
+        e = mesh.error(solution, uh0, errortype='max')
+        print(f"the max error is {e}")
+        return uh0, t
+fig, axes = plt.subplots()
+box = [0, 1, 0, 1, -1, 1] # 图像显示的范围 0 <= x <= 1, 0 <= y <= 1, -1 <= uh <= 1
+mesh.show_animation(fig, axes, box, advance_forward, fname='parabolic_af.mp4', plot_type='imshow', frames=nt + 1)
+plt.show()
+
